96714a917be8b196708e3561cee78e4dcebf46be
Gareth McMullin <gareth@blacksphere.co.nz>
2012-05-05 15:30:49 +1200

    Add support for logging to file and echo to gdb's stdout.

--- magictpa/__init__.py
+++ magictpa/__init__.py
@@ -157,6 +157,57 @@ class ParameterTpaGate(gdb.Parameter):
 			capture.pause()
 tpa_gate = ParameterTpaGate()
 
+class ParameterTpaEcho(gdb.Parameter):
+	"""Echo decoded trace to stdout"""
+	def __init__(self):
+		self.set_doc = "Echo decoded trace to stdout"
+		self.show_doc = "Echo decoded trace to stdout"
+		gdb.Parameter.__init__(self, "tpa echo", gdb.COMMAND_SUPPORT, 
+			gdb.PARAM_BOOLEAN)
+	def get_set_string(self):
+		if self.value:
+			return "Trace written to stdout"
+		else:
+			return "Trace not written to stdout"
+	def get_show_string(self, svalue):
+		if self.value:
+			return "Trace written to stdout"
+		else:
+			return "Trace not written to stdout"
+tpa_echo = ParameterTpaEcho()
+
+class ParameterTpaLog(gdb.Parameter):
+	"""TPA logfile"""
+	def __init__(self):
+		self.set_doc = "Record decoded trace stream to a file."
+		self.show_doc = "File decoded trace stream is captured to."
+		gdb.Parameter.__init__(self, "tpa log", gdb.COMMAND_SUPPORT, 
+			gdb.PARAM_OPTIONAL_FILENAME)
+		self.logfile = None
+	def get_set_string(self):
+		if self.logfile:
+			self.logfile.flush()
+		if self.value:
+			self.logfile = open(self.value, "a")
+			self.write("TPA logfile opened\n");
+
+		if self.value:
+			return "Logging trace stream to %s." % self.value
+		else:
+			return "Not logging trace stream."
+	def get_show_string(self, svalue):
+		if self.value:
+			return "Logging trace stream to %s." % self.value
+		else:
+			return "Not logging trace stream."
+	def write(self, event):
+		if self.value:
+			self.logfile.write(event)
+			self.logfile.flush()
+		if tpa_echo.value or not self.value:
+			gdb.write(event)
+tpa_log = ParameterTpaLog()
+
 class CommandTpaWatch(gdb.Command):
 	"""Trace a program variable"""
 	def __init__(self):
@@ -174,7 +225,7 @@ class CommandTpaWatch(gdb.Command):
 		if tpa_time.value == 'off':
 			time = ''
 		action = "%5s %s=%d" % (action, wp.varname, value)
-		print "%s %-25s %s" % (time, action, pc)
+		tpa_log.write("%s %-25s %s\n" % (time, action, pc))
 
 	def invoke(self, args, from_tty):
 		argv = gdb.string_to_argv(args)
